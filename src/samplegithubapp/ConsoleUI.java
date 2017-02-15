db74ccd336777cabb844804d60afbd6518bf0ae7
Debbie <Debbie@192.168.124.94>
2017-02-15 13:20:50 -0600

    Copied in info from gitignore.io

--- src/samplegithubapp/ConsoleUI.java
+++ src/samplegithubapp/ConsoleUI.java
@@ -4,5 +4,7 @@ public class ConsoleUI {
 public static void main (String[] args) {
     System.out.println("Hello, GitHub!");  
     System.out.println("I made a change");
+    System.out.println("I made a 2nd change");
+    
 } 
 }
