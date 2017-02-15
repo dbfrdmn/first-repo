138fda6351e67cdd0ebb6f9fab90e80b379fe1a9
Debbie <Debbie@192.168.124.94>
2017-02-15 13:23:15 -0600

    Copied in info from gitignore.io

--- src/samplegithubapp/ConsoleUI.java
+++ src/samplegithubapp/ConsoleUI.java
@@ -5,6 +5,8 @@ public static void main (String[] args) {
     System.out.println("Hello, GitHub!");  
     System.out.println("I made a change");
     System.out.println("I made a 2nd change");
+    System.out.println("I made a 3rd change");
+    
     
 } 
 }
